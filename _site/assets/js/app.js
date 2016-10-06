16b198e1fa65e27e031e85689f5efc41a2fad675
Nada Fadali <nada.fadali@gmail.com>
2016-10-06 12:27:39 +0300

    add favicon

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -3,17 +3,20 @@ $(function(){
 	// convert the contact form type to select.
 	if ($(window).width() < 453) {
 		$radio = $('form.contact>.form-group.radio');
-
 		var $select = $("<select id=\"type\" name=\"type\" />");
-
 		var data = ["Corporation", "Individual", "CSR", "Media", "NGO"];
 		for(var val in data) {
 		    $("<option />", {value: val, text: data[val]}).appendTo($select);
 		}
-
 		$($select).insertAfter($radio);
 		$radio.remove();
 	}
 
+
+	// contact form radio button
+	$('input[name="type"]').on('click', function(){
+		$(this).parent().siblings().removeClass('selected');
+		$(this).parent().addClass('selected');
+	});
 
 });
