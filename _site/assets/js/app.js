646a037d90a3826093ac3cf36eb9236c5e4d4218
Nada Fadali <nada.fadali@gmail.com>
2016-10-06 23:42:21 +0300

    sigh

--- _site/assets/js/app.js
+++ _site/assets/js/app.js
@@ -19,4 +19,33 @@ $(function(){
 		$(this).parent().addClass('selected');
 	});
 
+
+	$('form.contact#contact').on('submit', function(ev){
+		ev.preventDefault();
+
+		var data = {
+			'type': $('input[name=type]').val(),
+			'position': $('input[name=position]').val(),
+		  'first_name': $('input[name=first_name]').val(),
+		  'last_name': $('input[name=last_name]').val(),
+		  'message': $('input[name=message]').val()
+		};
+
+
+	// process the form
+	$.ajax({
+		type: 'POST',
+		url : '/scripts/contact.php',
+		data: data
+	}).done(function(data) {
+			// log data to the console so we can see
+			console.log(data);
+			// here we will handle errors and validation messages
+		});
+
+
+	});// end on form submit
+
+
+
 });
